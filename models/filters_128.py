696987a1ec84f038d7a52ff02fa0729536523b33
Henry <shenhengyu7@outlook.com>
2024-10-26 14:22:44 +0800

    Add files via upload

---
+++ models/filters_128.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 128, kernel_size=(5, 3), stride=(3, 1), dilation=(2, 1), padding=(12, 1)),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.MaxPool2d((2, 1), stride=(2, 1)),
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=(5, 3), stride=(3, 1), dilation=(2, 1), padding=(12, 1)),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.MaxPool2d((2, 1), stride=(2, 1)),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=(5, 3), stride=(3, 1), dilation=(2, 1), padding=(12, 1)),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.MaxPool2d((2, 1), stride=(2, 1)),
+        )
+        self.fc1 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(92160, 2),
+        )
+
+    def forward(self, x):
+        x = x.reshape(-1, 1, 64, 60)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = x.reshape(-1, 92160)
+        x = self.fc1(x)
+        return x
